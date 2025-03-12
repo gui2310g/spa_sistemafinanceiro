be18c521042cbcfc79d438eb404861c5b5863062
gui2310g <fontesguilherme57@gmail.com>
2025-03-12 01:07:07 -0300

    [add] added login, create and handleError in auth service

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoggedUser } from '../../models/login.model';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+import { User } from '../../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(LoggedUser: LoggedUser): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/auth`, LoggedUser).pipe(
+      tap((response) => sessionStorage.setItem('token', response.token)),
+      catchError(this.handleError)
+    );
+  }
+
+  create(User: User): Observable<User> {
+    return this.http
+      .post<User>(`${this.apiUrl}/usuarios`, User)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage: string;
+    if (error.status === 409) errorMessage = 'Email ou senha invalido';
+    if (error.status === 400) errorMessage = 'Email ja existente';
+    return throwError(() => new Error(errorMessage));
+  }
 }
