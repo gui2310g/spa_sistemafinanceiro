cae4ccc6c1a63ae3eeb8981fda8f90676e38dabc
gui2310g <fontesguilherme57@gmail.com>
2025-03-12 01:09:00 -0300

    [add,update] added forms, login and create in auth component, removed button in modal component

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,22 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalComponent } from '../../shared/components/modal/modal.component';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { LoggedUser } from '../../core/models/login.model';
+import { User } from '../../core/models/user.model';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ModalComponent, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   showModal: boolean = false;
+  loginForm!: FormGroup;
+  createForm!: FormGroup;
 
-  openDialog() {
-    this.showModal = true;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      senha: ['', Validators.required],
+    });
+
+    this.createForm = this.fb.group({
+      email: ['', Validators.required],
+      nome: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
-  closeDialog() {
-    this.showModal = false;
+  openDialog() { this.showModal = true }
+
+  closeDialog() { this.showModal = false; }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      const user: LoggedUser = this.loginForm.value;
+      this.authService.login(user).subscribe({
+        next: () => setTimeout(() => { this.router.navigate(['dashboard'])}, 1000),
+      });
+    }
+  }
+
+  create(): void {
+    if (this.createForm.valid) {
+      const user: User = this.createForm.value;
+      this.authService.create(user).subscribe({
+        next: () => setTimeout(() => { this.router.navigate(['dashboard']);}, 1000),
+      });
+    }
   }
 }
-
