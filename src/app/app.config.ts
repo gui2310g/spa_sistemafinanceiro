12dc9184b5caa300ddeba89c872d46f11e7acb0b
gui2310g <fontesguilherme57@gmail.com>
2025-03-12 01:07:43 -0300

    [add] created auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: authInterceptor, multi: true },
   ],
 };
---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class authInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const idToken = sessionStorage.getItem("token");
+
+    if(idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set("Authorization", idToken)
+      })
+
+      return next.handle(cloned)
+    }
+
+    return next.handle(req);
+  }
+};
